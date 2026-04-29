26aae4e8dd4af3d39627adefc2dccd71e004fccc
Michael Reichert <michael.reichert@geofabrik.de>
2026-04-29 17:11:40 +0200

    Remove GraphHopper brand icon and replace them by OpenRailRouting
    OpenRailRouting icon was traced in Inkscape from the raster version.

--- src/sidebar/PoweredBy.tsx
+++ src/sidebar/PoweredBy.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from '@/sidebar/PoweredBy.module.css'
-import Header from '@/sidebar/header.svg'
+import Logo from '@/sidebar/openrailrouting_logo.svg'
 import { tr } from '@/translation/Translation'
 
 export default function PoweredBy() {
     return (
         <div className={styles.poweredByContainer}>
             <span>{tr('powered_by')}</span>
-            <a className={styles.logoContainer} href="https://www.graphhopper.com" target="_blank">
-                <Header />
+            <a className={styles.logoContainer} href="https://routing.openrailrouting.org" target="_blank">
+	        <Logo />
             </a>
         </div>
     )
