4ba839fef100024aaf8e742d8e4f384be99d59c7
Michael Reichert <michael.reichert@geofabrik.de>
2026-04-29 18:43:43 +0200

    Make imprint, privacy and about link visible all the time

--- src/sidebar/PoweredBy.tsx
+++ src/sidebar/PoweredBy.tsx
@@ -2,14 +2,26 @@ import React from 'react'
 import styles from '@/sidebar/PoweredBy.module.css'
 import Logo from '@/sidebar/openrailrouting_logo.svg'
 import { tr } from '@/translation/Translation'
+import config from 'config'
 
 export default function PoweredBy() {
     return (
-        <div className={styles.poweredByContainer}>
-            <span>{tr('powered_by')}</span>
-            <a className={styles.logoContainer} href="https://routing.openrailrouting.org" target="_blank">
-	        <Logo />
-            </a>
-        </div>
+        <>
+            <div className={styles.poweredByContainer}>
+                <span>{tr('powered_by')}</span>
+                <a className={styles.logoContainer} href="https://routing.openrailrouting.org" target="_blank">
+                <Logo />
+                </a>
+            </div>
+            <div className={styles.infoLine}>
+                <a href="https://www.graphhopper.com/maps-route-planner/">{tr('info')}</a>
+                { config.docs && <a href={config.docs}>{tr('documentation')}</a> }
+                { config.backend_sources && <a href={config.backend_sources}>{tr('source code (backend)')}</a> }
+                { config.frontend_sources && <a href={config.frontend_sources}>{tr('source code (frontend)')}</a> }
+                { config.imprint && <a href={config.imprint}>{tr('imprint')}</a>}
+                { config.privacy && <a href={config.privacy}>{tr('privacy')}</a>}
+                { config.terms && <a href={config.terms}>{tr('terms')}</a>}
+            </div>
+        </>
     )
 }
--- src/sidebar/SettingsBox.tsx
+++ src/sidebar/SettingsBox.tsx
@@ -89,15 +89,6 @@ export default function SettingsBox({ profile }: { profile: RoutingProfile }) {
                     />
                 </div>
             </div>
-            <div className={styles.infoLine}>
-                <a href="https://www.graphhopper.com/maps-route-planner/">{tr('info')}</a>
-                { config.docs && <a href={config.docs}>{tr('documentation')}</a> }
-                { config.backend_sources && <a href={config.backend_sources}>{tr('source code (backend)')}</a> }
-                { config.frontend_sources && <a href={config.frontend_sources}>{tr('source code (frontend)')}</a> }
-                { config.imprint && <a href={config.imprint}>{tr('imprint')}</a>}
-                { config.privacy && <a href={config.privacy}>{tr('privacy')}</a>}
-                { config.terms && <a href={config.terms}>{tr('terms')}</a>}
-            </div>
         </div>
     )
 }
