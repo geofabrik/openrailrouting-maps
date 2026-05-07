2422afc2aa73a3727d69dfae9b291218d017bced
Michael Reichert <michael.reichert@geofabrik.de>
2026-05-07 18:12:07 +0200

    Use a select instead of a list of icons for profile selection

--- config.js
+++ config.js
@@ -97,27 +97,8 @@ const config = {
         },
     ],
 
-    // You can 'collapse' or group certain profiles to reduce the number of profiles in the panel. Instead they're listed in the settings but still a profile icon is shown.
-    // Note: the name of the group must be the default option for this group.
+    // Profile group mapping is not supported by OpenRailRouting Maps.
     profile_group_mapping: {},
-    // profile_group_mapping: {
-    //  car: {
-    //    options: [
-    //      { profile: 'car' },
-    //      { profile: 'car_avoid_motorway' },
-    //      { profile: 'car_avoid_ferry' },
-    //      { profile: 'car_avoid_toll' }
-    //    ]
-    //  },
-    //  bike: {
-    //    options: [
-    //      { profile: 'bike' },
-    //      { profile: 'mtb' },
-    //      { profile: 'racingbike' },
-    //      { profile: 'ecargobike' }
-    //    ]
-    //  }
-    // }
 }
 
 // this is needed for jest (with our current setup at least)
--- src/sidebar/search/routingProfiles/RoutingProfiles.tsx
+++ src/sidebar/search/routingProfiles/RoutingProfiles.tsx
@@ -4,12 +4,10 @@ import Dispatcher from '@/stores/Dispatcher'
 import { SetVehicleProfile, SetVehicleProfileGroup } from '@/actions/Actions'
 import { RoutingProfile } from '@/api/graphhopper'
 import PlainButton from '@/PlainButton'
-import Chevron from './chevron.svg'
 import { tr } from '@/translation/Translation'
 import CustomModelBoxSVG from '@/sidebar/open_custom_model.svg'
 import { icons } from '@/sidebar/search/routingProfiles/profileIcons'
 import * as config from 'config'
-import { ProfileGroupMap } from '@/utils'
 
 export default function ({
     routingProfiles,
@@ -26,65 +24,7 @@ export default function ({
     toggleCustomModelBox: () => void
     customModelBoxEnabled: boolean
 }) {
-    const [profileScroll, setProfileScroll] = useState(0)
-    const [profileWidth, setProfileWidth] = useState(0)
 
-    function handleResize() {
-        const profilesCarouselItems = document.getElementById('profiles_carousel_items')
-        if (!profilesCarouselItems) return
-        if (profilesCarouselItems.scrollWidth > profilesCarouselItems.clientWidth) {
-            for (const chevron of document.getElementsByClassName(styles.chevron)) {
-                chevron.classList.add(styles.enabled)
-            }
-            profilesCarouselItems.classList.remove(styles.profiles_center)
-        } else {
-            for (const chevron of document.getElementsByClassName(styles.chevron)) {
-                chevron.classList.remove(styles.enabled)
-            }
-            profilesCarouselItems.classList.add(styles.profiles_center)
-        }
-        setProfileWidth(profilesCarouselItems.scrollWidth - profilesCarouselItems.clientWidth)
-    }
-
-    useEffect(() => {
-        handleResize()
-        // If the window is resized, we need to check if the chevrons should be enabled
-        // and if the profiles should be centered
-        window.addEventListener('resize', handleResize)
-        return () => window.removeEventListener('resize', handleResize)
-    }, [routingProfiles])
-
-    function move(forward: boolean = true) {
-        const profilesCarouselItems = document.getElementById('profiles_carousel_items')
-        if (!profilesCarouselItems) return
-
-        const scrollAmount = profilesCarouselItems.clientWidth * (forward ? 1 : -1)
-        const limit = forward ? profileWidth - profileScroll : -profileScroll
-        const scrollValue = forward ? Math.min(scrollAmount, limit) : Math.max(scrollAmount, limit)
-
-        profilesCarouselItems.scrollBy({
-            left: scrollValue,
-            behavior: 'smooth',
-        })
-    }
-
-    function onScroll() {
-        const profilesCarouselItems = document.getElementById('profiles_carousel_items')
-        if (!profilesCarouselItems) return
-        setProfileScroll(profilesCarouselItems.scrollLeft)
-    }
-
-    // Create a hash to count custom profiles to later enhance the base icon with a number.
-    let customProfiles: Record<string, Array<string>> = {}
-
-    // Now add the profiles to each key e.g. 'car_avoid_ferry' to the 'car_' key.
-    // Create a special key '_' for profile names with an unknown icon.
-    routingProfiles.forEach(p => {
-        const key = (Object.keys(icons).find(k => p.name.startsWith(k + '_')) || '') + '_'
-        if (!icons[p.name]) customProfiles[key] = [...(customProfiles[key] || []), p.name]
-    })
-
-    let profileToGroup = ProfileGroupMap.create(config.profile_group_mapping)
     return (
         <div className={styles.profilesParent}>
             <PlainButton
@@ -94,97 +34,35 @@ export default function ({
             >
                 <CustomModelBoxSVG />
             </PlainButton>
-            <div className={styles.carousel}>
-                <PlainButton
-                    className={styles.chevron}
-                    title={tr('back')}
-                    onClick={() => move(false)}
-                    disabled={profileScroll <= 0}
-                >
-                    <Chevron />
-                </PlainButton>
-                <ul className={styles.profiles} id="profiles_carousel_items" onScroll={onScroll}>
+            <div className={styles.routingProfiles}>
+                <select className={styles.profiles} id="profiles_carousel_items"
+                    onChange={(e) => {
+                            const profile = routingProfiles.find(p => p.name === e.target.value)
+                            if (profile)
+                                Dispatcher.dispatch(new SetVehicleProfile(profile))
+                        }
+                    }>
                     {routingProfiles
-                        .filter(
-                            profile => !profileToGroup[profile.name] || profile.name == profileToGroup[profile.name],
-                        )
                         .map(profile => {
-                            const groupName = profileToGroup[profile.name]
                             const isProfileSelected =
-                                profile.name === selectedProfile.name ||
-                                profile.name == profileToGroup[selectedProfile.name]
+                                profile.name === selectedProfile.name
                             const className = isProfileSelected
                                 ? styles.selectedProfile + ' ' + styles.profileBtn
                                 : styles.profileBtn
-                            // For group leaders, show the memorized profile's icon if available
-                            const activeProfileName = groupName
-                                ? memorizedProfilePerGroup[groupName] || profile.name
-                                : profile.name
-                            const iconName = icons[activeProfileName] ? activeProfileName : profile.name
                             return (
-                                <li key={profile.name}>
-                                    <PlainButton
-                                        title={tr(activeProfileName)}
-                                        onClick={() =>
-                                            Dispatcher.dispatch(
-                                                groupName
-                                                    ? new SetVehicleProfileGroup(groupName)
-                                                    : new SetVehicleProfile(profile),
-                                            )
-                                        }
+                                <option value={profile.name}
                                         className={className}
-                                    >
-                                        {customModelBoxEnabled && profile.name === selectedProfile.name && (
-                                            <CustomModelBoxSVG className={styles.asIndicator} />
-                                        )}
-                                        {getIcon(iconName, customProfiles)}
-                                    </PlainButton>
-                                </li>
+                                  //  >
+                                  //      {customModelBoxEnabled && profile.name === selectedProfile.name && (
+                                  //          <CustomModelBoxSVG className={styles.asIndicator} />
+                                  //      )}
+                                >
+                                        {tr(profile.name)}
+                                </option>
                             )
                         })}
-                </ul>
-                <PlainButton
-                    className={styles.chevron + ' ' + styles.flip}
-                    title={tr('next')}
-                    onClick={() => move()}
-                    disabled={profileScroll >= profileWidth}
-                >
-                    <Chevron />
-                </PlainButton>
+                </select>
             </div>
         </div>
     )
-}
-
-function getIcon(profileName: string, customProfiles: Record<string, Array<string>>) {
-    let icon = icons[profileName]
-    if (icon) return React.createElement(icon)
-
-    // go through every key in customProfiles and check if the profile is in the array under that key
-    for (const [key, value] of Object.entries(customProfiles)) {
-        const index = value.findIndex(p => p == profileName) + 1
-        if (index >= 1) {
-            let icon = icons[key.slice(0, -1)] // slice to remove underscore from key
-            if (!icon) icon = icons.question_mark
-            return key === '_' ? <NumberIcon number={index} /> : <IconWithBatchNumber baseIcon={icon} number={index} />
-        }
-    }
-
-    // this is the very last fallback, should never be reached
-    return React.createElement(icons.question_mark)
-}
-
-function IconWithBatchNumber({ baseIcon, number }: { baseIcon: any; number: number }) {
-    return (
-        <div className={styles.iconContainer}>
-            {React.createElement(baseIcon)}
-            <div className={styles.batchNumber}>
-                <NumberIcon number={number} />
-            </div>
-        </div>
-    )
-}
-
-function NumberIcon({ number }: { number: number }) {
-    return <span>{number}</span>
 }
